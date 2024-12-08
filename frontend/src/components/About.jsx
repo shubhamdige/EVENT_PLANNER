c8a79e0e0a555b2592938ee773572ddbe63b9bfa
Your Name <your.email@example.com>
2024-12-08 21:38:52 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>Welcome to Event Horizon, your one-stop solution for effortless event planning! Whether it’s a 
+        wedding, birthday, corporate gathering, or any special occasion, we are here to turn your vision 
+        into reality. Our platform simplifies event organization by connecting you with trusted vendors, 
+        managing schedules, and tracking budgets seamlessly. With user-friendly features and personalized 
+        planning tools, we ensure that every detail is handled with precision and care. Our mission is to 
+        make planning stress-free and enjoyable so you can focus on creating unforgettable memories. 
+        Let’s bring your dream events to life, one celebration at a time!
+      </p>
+    </section>
+  )
+}
+
+export default About;
